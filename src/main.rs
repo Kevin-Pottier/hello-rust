638b6c60860a6708c3c7ff3c5f17e6ebd99a9928
Kevin Pottier <kevin.pottier@eseo.fr>
2025-09-04 16:58:21 +0200

    Added random secret number generation

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Hello, world!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
     println!("Please input your guess : ");
     let mut guess = String::new();
 
     io::stdin().read_line(&mut guess).expect("Failed to read line");
 
-    println!("You guessed: {}", guess);
+    if guess == secret_number.to_string() {
+        println!("You win!");
+    } else {
+        println!("You lose! The secret number was: {}", secret_number);
+    }
+    
 }
