0ff03422a001ff0aa3189ff27b3ada2635d7a2f7
Kevin Pottier <kevin.pottier@eseo.fr>
2025-09-05 11:50:54 +0200

    Added memory overflow and structure print error

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use std::io;
+use std::mem;
 use rand::Rng;
 
 fn main() {
+    //#[allow(dead_code)]
     println!("Hello, world!");
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
@@ -17,4 +19,15 @@ fn main() {
         println!("You lose! The secret number was: {}", secret_number);
     }
     
+    struct Structure(i32);
+
+    //println!("This struct `{:?}` won't print...", Structure(3));    
+
+    let number: u32 = 30; 
+    //let number2: u8 = 256; // i8 is not large enough to hold 256
+    println!("number is: {number}");
+    //println!("number2 is: {}", number2);
+    println!("Max u8: {}, min u8: {}", std::u8::MAX, std::u8::MIN);
+    //println!("but size of number2 is: {} bytes", mem::size_of_val(&number2));
+
 }
